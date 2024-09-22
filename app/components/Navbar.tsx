f7acf6b0b3a44e32d7a687da9e7d30e77ff21710
Byron Njiaju <chikenjiaju@gmail.com>
2024-09-22 17:07:30 +0400

    data table

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Chat', href: '/chat', current: true },
-  { name: 'Team', href: '#', current: false },
+  { name: 'People', href: '/people', current: false },
   { name: 'Projects', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
 ]
